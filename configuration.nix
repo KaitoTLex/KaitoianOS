dffe288ead1c87ea8a587fbf2d5aa7e76f72e3fa
Ren Lawrence Lin <70042943+KaitoTLex@users.noreply.github.com>
2024-09-03 18:10:20 -0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -10,8 +10,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    nixvim.nixosModules.nixvim
-  ];
+];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
