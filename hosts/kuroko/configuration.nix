98e03b3846b2ba0311daf68330d2d64def0a22cd
KaitoTLex <warrenlin1688@gmail.com>
2024-09-29 20:33:55 -0700

    saged

--- hosts/kuroko/configuration.nix
+++ hosts/kuroko/configuration.nix
@@ -11,13 +11,44 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
-
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   virtualisation.waydroid.enable = true;
   networking.hostName = "kuroko"; # Define your hostname.
 
+  #Nvidia Hardware begins 
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = true;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
@@ -46,12 +77,18 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
+  hardware.nvidia.prime = {
+    # Make sure to use the correct Bus ID values for your system!
+    #intelBusId = "PCI:";
+    nvidiaBusId = "PCI:01:0:0";
+    amdgpuBusId = "PCI:08:00:00";
+  };
   services.fprintd = {
     enable = true;
     package = pkgs.fprintd-tod;
     tod = {
-      enable = true;
-      driver = pkgs.libfprint-2-tod1-vfs0090;
+      enable = false;
+      driver = pkgs.libfprint-2-tod1-elan;
     };
   };
   # List services that you want to enable:
