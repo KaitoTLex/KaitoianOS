737042f363cfd1b13358cc4b97dbbacad7c5b197
Youwen Wu <youwenw@gmail.com>
2024-09-30 00:09:33 -0700

    feat!: replace sddm with greetd

--- modules/wm/default.nix
+++ modules/wm/default.nix
@@ -1,7 +1,24 @@
+{ pkgs, ... }:
 {
-  services.displayManager.sddm = {
+  services.greetd = {
     enable = true;
-    wayland.enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland --remember --greeting 'Access is restricted to authorized personnel only.'";
+        user = "greeter";
+      };
+    };
+  };
+
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
   };
 
   programs.sway = {
