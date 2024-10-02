f8026d47da0d8518009ea4a0a4c862e8287e3d0c
KaitoTLex <warrenlin1688@gmail.com>
2024-10-02 09:06:12 -0700

    notsoclean

--- users/kaitotlex/home.nix
+++ users/kaitotlex/home.nix
@@ -167,6 +167,7 @@
     userEmail = "warrenlin1688@gmail.com";
     delta.enable = true;
     extraConfig = {
+      credential.helper = "${pkgs.git.override { withLibsecret = true; }}/bin/git-credential-libsecret";
       init.defaultBranch = "main";
     };
   };
