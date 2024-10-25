6daaef002566044ec1bf84f2800befda63c6ef14
KaitoTLex <warrenlin1688@gmail.com>
2024-10-24 19:38:17 -0700

    iwannacry

--- users/kaitotlex/home.nix
+++ users/kaitotlex/home.nix
@@ -22,7 +22,7 @@
   home.packages = with pkgs; [
     # here is some command line tools I use frequently
     # feel free to add your own or remove some of them
-
+    typst-live
     typst
     tmux
     pipes
