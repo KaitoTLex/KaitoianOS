86f5f6fb94d524337001e464ea16ee187acb8d37
KaitoTLex <warrenlin1688@gmail.com>
2025-01-20 17:52:47 -0800

    no

--- users/kaitotlex/home.nix
+++ users/kaitotlex/home.nix
@@ -22,6 +22,7 @@
   home.packages = with pkgs; [
     # here is some command line tools I use frequently
     # feel free to add your own or remove some of them
+    tor
     libreoffice
     rasm
     arduino-language-server
