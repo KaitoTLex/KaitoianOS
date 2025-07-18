6f0ae20674b9c99876dcbb14e8c7f7d037cc32d5
KaitoTLex <renl@kaitotlex.systems>
2025-07-18 13:22:30 -0700

    IRL TESTING

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       # url = "github:zzywysm/nixos-asahi";
       url = "github:nix-community/nixos-apple-silicon";
       inputs.nixpkgs.follows = "nixpkgs";
-    }; 
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -46,7 +46,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  
+
   outputs =
     {
       nixpkgs,
@@ -55,302 +55,303 @@
       ...
     }@inputs:
     let
-      mkPkgs = system: let
-        pkgs = import nixpkgs { inherit system; };
-        packages = import ./pkgs/byArch { inherit pkgs system; };
-      in packages;
-    in 
+      mkPkgs =
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          packages = import ./pkgs/byArch { inherit pkgs system; };
+        in
+        packages;
+    in
     {
       nixosConfigurations = {
         kuroko = nixpkgs.lib.nixosSystem {
-        specialArgs = {
-          inherit inputs;
-        };
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/kuroko
-          lanzaboote.nixosModules.lanzaboote
-          (
-            { pkgs, lib, ... }:
+          specialArgs = {
+            inherit inputs;
+          };
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/kuroko
+            lanzaboote.nixosModules.lanzaboote
+            (
+              { pkgs, lib, ... }:
+              {
+                environment.systemPackages = mkPkgs "x86_64-linux";
+                # Lanzaboote currently replaces the systemd-boot module.
+                # This setting is usually set to true in configuration.nix
+                # generated at installation time. So we force it to false
+                # for now.
+                boot.loader.systemd-boot.enable = lib.mkForce false;
+
+                boot.lanzaboote = {
+                  enable = true;
+                  pkiBundle = "/var/lib/sbctl";
+                };
+              }
+            )
+            home-manager.nixosModules.home-manager
             {
-              environment.systemPackages = mkPkgs "x86_64-linux";
-              # Lanzaboote currently replaces the systemd-boot module.
-              # This setting is usually set to true in configuration.nix
-              # generated at installation time. So we force it to false
-              # for now.
-              boot.loader.systemd-boot.enable = lib.mkForce false;
-
-              boot.lanzaboote = {
-                enable = true;
-                pkiBundle = "/var/lib/sbctl";
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                backupFileExtension = "backup";
+                extraSpecialArgs = {
+                  inherit inputs;
+                };
+                users.kaitotlex = {
+                  imports = [
+                    ./users/kaitotlex
+                    {
+                      wayland.windowManager.hyprland.settings.monitor = [
+                        "eDP-1,1920x1200@120,0x0,1"
+                        "DP-1, 1920x1080@75.03,3840x0,1,transform, 1"
+                        "HDMI-A-1,1920x1080@165,1920x0,1"
+                      ];
+                      programs.git.signing = {
+                        signByDefault = true;
+                        key = "42F52D76F1B15B8D997E2AEE8AB934746F475D0B";
+                      };
+                    }
+                  ];
+                };
               };
             }
-          )
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              backupFileExtension = "backup";
-              extraSpecialArgs = {
-                inherit inputs;
-              };
-              users.kaitotlex = {
-                imports = [
-                  ./users/kaitotlex
-                  {
-                    wayland.windowManager.hyprland.settings.monitor = [
-                      "eDP-1,1920x1200@120,0x0,1"
-                      "DP-1, 1920x1080@75.03,3840x0,1,transform, 1"
-                      "HDMI-A-1,1920x1080@165,1920x0,1"
-                    ];
-                    programs.git.signing = {
-                      signByDefault = true;
-                      key = "42F52D76F1B15B8D997E2AEE8AB934746F475D0B";
-                    };
-                  }
-                ];
-              };
-            };
-          }
-        ];
-      };
-      shiroko = nixpkgs.lib.nixosSystem {
-        specialArgs = {
-          inherit inputs;
+          ];
         };
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/shiroko
-          lanzaboote.nixosModules.lanzaboote
+        shiroko = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit inputs;
+          };
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/shiroko
+            lanzaboote.nixosModules.lanzaboote
 
-          (
-            { pkgs, lib, ... }:
+            (
+              { pkgs, lib, ... }:
+              {
+
+                environment.systemPackages = mkPkgs "x86_64-linux";
+                # Lanzaboote currently replaces the systemd-boot module.
+                # This setting is usually set to true in configuration.nix
+                # generated at installation time. So we force it to false
+                # for now.
+                boot.loader.systemd-boot.enable = lib.mkForce false;
+
+                boot.lanzaboote = {
+                  enable = true;
+                  pkiBundle = "/var/lib/sbctl";
+                };
+              }
+            )
+            home-manager.nixosModules.home-manager
             {
-
-              
-              environment.systemPackages = mkPkgs "x86_64-linux";
-              # Lanzaboote currently replaces the systemd-boot module.
-              # This setting is usually set to true in configuration.nix
-              # generated at installation time. So we force it to false
-              # for now.
-              boot.loader.systemd-boot.enable = lib.mkForce false;
-
-              boot.lanzaboote = {
-                enable = true;
-                pkiBundle = "/var/lib/sbctl";
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                backupFileExtension = "backup";
+                extraSpecialArgs = {
+                  inherit inputs;
+                };
+                users.kaitotlex = {
+                  imports = [
+                    ./users/kaitotlex
+                    {
+                      wayland.windowManager.hyprland.settings.monitor = [
+                        "eDP-1,1920x1200@120,0x0,1"
+                        "DP-1, 1920x1080@144.04,1920x0,1"
+                      ];
+                      programs.git.signing = {
+                        signByDefault = true;
+                        key = "BC04C0C14AEDA705B8FBACE8C5F52A3C0F3B4A77";
+                      };
+                    }
+                  ];
+                };
               };
             }
-          )
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              backupFileExtension = "backup";
-              extraSpecialArgs = {
-                inherit inputs;
-              };
-              users.kaitotlex = {
-                imports = [
-                  ./users/kaitotlex
-                  {
-                    wayland.windowManager.hyprland.settings.monitor = [
-                      "eDP-1,1920x1200@120,0x0,1"
-                      "DP-1, 1920x1080@144.04,1920x0,1"
-                    ];
-                    programs.git.signing = {
-                      signByDefault = true;
-                      key = "BC04C0C14AEDA705B8FBACE8C5F52A3C0F3B4A77";
-                    };
-                  }
-                ];
-              };
-            };
-          }
-        ];
-      };
-      kanade = nixpkgs.lib.nixosSystem {
-        specialArgs = {
-          inherit inputs;
+          ];
         };
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/kanade
-          (
-            { pkgs, lib, ... }:
-            {environment.systemPackages = mkPkgs "aarch64-linux";
+        kanade = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit inputs;
+          };
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/kanade
+            (
+              { pkgs, lib, ... }:
+              {
+                environment.systemPackages = mkPkgs "aarch64-linux";
+              }
+            )
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                backupFileExtension = "backup";
+                extraSpecialArgs = {
+                  inherit inputs;
+                };
+                users.kaitotlex = {
+                  imports = [
+                    ./users/kaitotlex
+                    {
+                      wayland.windowManager.hyprland.settings.monitor = [
+                        "eDP-1,1920x1200@120,0x0,1.25"
+                      ];
+                    }
+                  ];
+                };
+              };
             }
-          )
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              backupFileExtension = "backup";
-              extraSpecialArgs = {
-                inherit inputs;
-              };
-              users.kaitotlex = {
-                imports = [
-                  ./users/kaitotlex
-                  {
-                    wayland.windowManager.hyprland.settings.monitor = [
-                      "eDP-1,1920x1200@120,0x0,1"
-                      "DP-1, 1920x1080@144.04,1920x0,1"
-                    ];
-                  }
-                ];
-              };
-            };
-          }
-        ];
-      }; 
+          ];
+        };
       };
     };
 }
 
+#formatter."x86_64-linux" = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+#formatter."aarch64-linux" = nixpkgs.legacyPackages.aarch64-linux.nixfmt-rfc-style;
+# nixosConfigurations.kuroko = nixpkgs.lib.nixosSystem {
+#   specialArgs = {
+#     inherit inputs;
+#   };
+#   system = "x86_64-linux";
+#   modules = [
+#     ./hosts/kuroko
+#     lanzaboote.nixosModules.lanzaboote
 
-      #formatter."x86_64-linux" = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
-      #formatter."aarch64-linux" = nixpkgs.legacyPackages.aarch64-linux.nixfmt-rfc-style;
-      # nixosConfigurations.kuroko = nixpkgs.lib.nixosSystem {
-      #   specialArgs = {
-      #     inherit inputs;
-      #   };
-      #   system = "x86_64-linux";
-      #   modules = [
-      #     ./hosts/kuroko
-      #     lanzaboote.nixosModules.lanzaboote
+#     (
+#       { pkgs, lib, ... }:
+#       {
 
-      #     (
-      #       { pkgs, lib, ... }:
-      #       {
+#         environment.systemPackages = [
+#           # For debugging and troubleshooting Secure Boot.
+#           pkgs.sbctl
+#         ];
 
-      #         environment.systemPackages = [
-      #           # For debugging and troubleshooting Secure Boot.
-      #           pkgs.sbctl
-      #         ];
+#         # Lanzaboote currently replaces the systemd-boot module.
+#         # This setting is usually set to true in configuration.nix
+#         # generated at installation time. So we force it to false
+#         # for now.
+#         boot.loader.systemd-boot.enable = lib.mkForce false;
 
-      #         # Lanzaboote currently replaces the systemd-boot module.
-      #         # This setting is usually set to true in configuration.nix
-      #         # generated at installation time. So we force it to false
-      #         # for now.
-      #         boot.loader.systemd-boot.enable = lib.mkForce false;
+#         boot.lanzaboote = {
+#           enable = true;
+#           pkiBundle = "/var/lib/sbctl";
+#         };
+#       }
+#     )
+#     home-manager.nixosModules.home-manager
+#     {
+#       home-manager = {
+#         useGlobalPkgs = true;
+#         useUserPackages = true;
+#         backupFileExtension = "backup";
+#         extraSpecialArgs = {
+#           inherit inputs;
+#         };
+#         users.kaitotlex = {
+#           imports = [
+#             ./users/kaitotlex
+#             {
+#               wayland.windowManager.hyprland.settings.monitor = [
+#                 "eDP-1,1920x1200@120,0x0,1"
+#                 "DP-1, 1920x1080@75.03,3840x0,1,transform, 1"
+#                 "HDMI-A-1,1920x1080@165,1920x0,1"
+#               ];
+#               programs.git.signing = {
+#                 signByDefault = true;
+#                 key = "42F52D76F1B15B8D997E2AEE8AB934746F475D0B";
+#               };
+#             }
+#           ];
+#         };
+#       };
+#     }
+#   ];
+# };
+# nixosConfigurations.shiroko = nixpkgs.lib.nixosSystem {
+#   specialArgs = {
+#     inherit inputs;
+#   };
+#   system = "x86_64-linux";
+#   modules = [
+#     ./hosts/shiroko
+#     lanzaboote.nixosModules.lanzaboote
 
-      #         boot.lanzaboote = {
-      #           enable = true;
-      #           pkiBundle = "/var/lib/sbctl";
-      #         };
-      #       }
-      #     )
-      #     home-manager.nixosModules.home-manager
-      #     {
-      #       home-manager = {
-      #         useGlobalPkgs = true;
-      #         useUserPackages = true;
-      #         backupFileExtension = "backup";
-      #         extraSpecialArgs = {
-      #           inherit inputs;
-      #         };
-      #         users.kaitotlex = {
-      #           imports = [
-      #             ./users/kaitotlex
-      #             {
-      #               wayland.windowManager.hyprland.settings.monitor = [
-      #                 "eDP-1,1920x1200@120,0x0,1"
-      #                 "DP-1, 1920x1080@75.03,3840x0,1,transform, 1"
-      #                 "HDMI-A-1,1920x1080@165,1920x0,1"
-      #               ];
-      #               programs.git.signing = {
-      #                 signByDefault = true;
-      #                 key = "42F52D76F1B15B8D997E2AEE8AB934746F475D0B";
-      #               };
-      #             }
-      #           ];
-      #         };
-      #       };
-      #     }
-      #   ];
-      # };
-      # nixosConfigurations.shiroko = nixpkgs.lib.nixosSystem {
-      #   specialArgs = {
-      #     inherit inputs;
-      #   };
-      #   system = "x86_64-linux";
-      #   modules = [
-      #     ./hosts/shiroko
-      #     lanzaboote.nixosModules.lanzaboote
+#     (
+#       { pkgs, lib, ... }:
+#       {
 
-      #     (
-      #       { pkgs, lib, ... }:
-      #       {
+#         environment.systemPackages = [
+#           # For debugging and troubleshooting Secure Boot.
+#           pkgs.sbctl
+#         ];
 
-      #         environment.systemPackages = [
-      #           # For debugging and troubleshooting Secure Boot.
-      #           pkgs.sbctl
-      #         ];
+#         # Lanzaboote currently replaces the systemd-boot module.
+#         # This setting is usually set to true in configuration.nix
+#         # generated at installation time. So we force it to false
+#         # for now.
+#         boot.loader.systemd-boot.enable = lib.mkForce false;
 
-      #         # Lanzaboote currently replaces the systemd-boot module.
-      #         # This setting is usually set to true in configuration.nix
-      #         # generated at installation time. So we force it to false
-      #         # for now.
-      #         boot.loader.systemd-boot.enable = lib.mkForce false;
+#         boot.lanzaboote = {
+#           enable = true;
+#           pkiBundle = "/var/lib/sbctl";
+#         };
+#       }
+#     )
+#     home-manager.nixosModules.home-manager
+#     {
+#       home-manager = {
+#         useGlobalPkgs = true;
+#         useUserPackages = true;
+#         backupFileExtension = "backup";
+#         extraSpecialArgs = {
+#           inherit inputs;
+#         };
+#         users.kaitotlex = {
+#           imports = [
+#             ./users/kaitotlex
+#             {
+#               wayland.windowManager.hyprland.settings.monitor = [
+#                 "eDP-1,1920x1200@120,0x0,1"
+#                 "DP-1, 1920x1080@144.04,1920x0,1"
+#               ];
+#               programs.git.signing = {
+#                 signByDefault = true;
+#                 key = "BC04C0C14AEDA705B8FBACE8C5F52A3C0F3B4A77";
+#               };
+#             }
+#           ];
+#         };
+#       };
+#     }
+#   ];
+# };
+# nixosConfigurations.kurokoNightly = nixpkgs.lib.nixosSystem {
+#   specialArgs = {
+#     inherit inputs;
+#   };
+#   system = "x86_64-linux";
+#   modules = [
+#     ./hosts/kurokoNightly
 
-      #         boot.lanzaboote = {
-      #           enable = true;
-      #           pkiBundle = "/var/lib/sbctl";
-      #         };
-      #       }
-      #     )
-      #     home-manager.nixosModules.home-manager
-      #     {
-      #       home-manager = {
-      #         useGlobalPkgs = true;
-      #         useUserPackages = true;
-      #         backupFileExtension = "backup";
-      #         extraSpecialArgs = {
-      #           inherit inputs;
-      #         };
-      #         users.kaitotlex = {
-      #           imports = [
-      #             ./users/kaitotlex
-      #             {
-      #               wayland.windowManager.hyprland.settings.monitor = [
-      #                 "eDP-1,1920x1200@120,0x0,1"
-      #                 "DP-1, 1920x1080@144.04,1920x0,1"
-      #               ];
-      #               programs.git.signing = {
-      #                 signByDefault = true;
-      #                 key = "BC04C0C14AEDA705B8FBACE8C5F52A3C0F3B4A77";
-      #               };
-      #             }
-      #           ];
-      #         };
-      #       };
-      #     }
-      #   ];
-      # };
-      # nixosConfigurations.kurokoNightly = nixpkgs.lib.nixosSystem {
-      #   specialArgs = {
-      #     inherit inputs;
-      #   };
-      #   system = "x86_64-linux";
-      #   modules = [
-      #     ./hosts/kurokoNightly
-
-      #     home-manager.nixosModules.home-manager
-      #     {
-      #       home-manager = {
-      #         useGlobalPkgs = true;
-      #         useUserPackages = true;
-      #         backupFileExtension = "backup";
-      #         extraSpecialArgs = {
-      #           inherit inputs;
-      #         };
-      #         users.kaitotlex = {
-      #           imports = [ ./users/kaitotlex ];
-      #         };
-      #       };
-      #     }
-      #   ];
-      # };
+#     home-manager.nixosModules.home-manager
+#     {
+#       home-manager = {
+#         useGlobalPkgs = true;
+#         useUserPackages = true;
+#         backupFileExtension = "backup";
+#         extraSpecialArgs = {
+#           inherit inputs;
+#         };
+#         users.kaitotlex = {
+#           imports = [ ./users/kaitotlex ];
+#         };
+#       };
+#     }
+#   ];
+# };
